f39f31b39f89364f2f68db237e022d5afc32d4d0
himanshu <sahu.himanshu2016@gmail.com>
2023-02-25 00:58:46 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 
 function App() {
+  const[markdown ,setmarkdown] = useState('');
+
+  const handchange = (event) =>{
+    setmarkdown(event.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='left'>
+      <h3>Enter a markdown data</h3>
+    <textarea value={markdown} onChange={handchange} />
+    </div>
+    
+     {console.log(markdown)}
+     <div className='right'>
+      <h1>Markdown Result </h1>
+     <ReactMarkdown >{markdown}</ReactMarkdown>
+     </div>
+     
     </div>
   );
 }
